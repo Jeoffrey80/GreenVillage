da10452cc2cb7b2fe89c76518a2692f1e589294a
Jeoffrey80 <127749877+Jeoffrey80@users.noreply.github.com>
2024-06-26 13:49:46 +0200

    creation entité commande facture + jeu de test fixture client

---
+++ src/Entity/Commande.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommandeRepository::class)]
+class Commande
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_commande = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $montant_total = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $statut = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateCommande(): ?\DateTimeInterface
+    {
+        return $this->date_commande;
+    }
+
+    public function setDateCommande(\DateTimeInterface $date_commande): static
+    {
+        $this->date_commande = $date_commande;
+
+        return $this;
+    }
+
+    public function getMontantTotal(): ?string
+    {
+        return $this->montant_total;
+    }
+
+    public function setMontantTotal(string $montant_total): static
+    {
+        $this->montant_total = $montant_total;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Facture.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FactureRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FactureRepository::class)]
+class Facture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $montant = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_emission = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMontant(): ?string
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(string $montant): static
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    public function getDateEmission(): ?\DateTimeInterface
+    {
+        return $this->date_emission;
+    }
+
+    public function setDateEmission(\DateTimeInterface $date_emission): static
+    {
+        $this->date_emission = $date_emission;
+
+        return $this;
+    }
+}

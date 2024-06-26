c39da8f8d70260873ed8762a3977c7af88408c53
Jeoffrey80 <127749877+Jeoffrey80@users.noreply.github.com>
2024-06-26 13:14:01 +0200

    màj

---
+++ src/Entity/Client.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClientRepository::class)]
+class Client
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adresse_mail = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adresse_livraison = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adresse_facuration = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $role = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    #[ORM\Column]
+    private ?bool $typeclient = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $coefficient = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $reduction_professionnelle = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getAdresseMail(): ?string
+    {
+        return $this->adresse_mail;
+    }
+
+    public function setAdresseMail(string $adresse_mail): static
+    {
+        $this->adresse_mail = $adresse_mail;
+
+        return $this;
+    }
+
+    public function getAdresseLivraison(): ?string
+    {
+        return $this->adresse_livraison;
+    }
+
+    public function setAdresseLivraison(string $adresse_livraison): static
+    {
+        $this->adresse_livraison = $adresse_livraison;
+
+        return $this;
+    }
+
+    public function getAdresseFacuration(): ?string
+    {
+        return $this->adresse_facuration;
+    }
+
+    public function setAdresseFacuration(string $adresse_facuration): static
+    {
+        $this->adresse_facuration = $adresse_facuration;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): static
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function isTypeclient(): ?bool
+    {
+        return $this->typeclient;
+    }
+
+    public function setTypeclient(bool $typeclient): static
+    {
+        $this->typeclient = $typeclient;
+
+        return $this;
+    }
+
+    public function getCoefficient(): ?string
+    {
+        return $this->coefficient;
+    }
+
+    public function setCoefficient(string $coefficient): static
+    {
+        $this->coefficient = $coefficient;
+
+        return $this;
+    }
+
+    public function getReductionProfessionnelle(): ?string
+    {
+        return $this->reduction_professionnelle;
+    }
+
+    public function setReductionProfessionnelle(string $reduction_professionnelle): static
+    {
+        $this->reduction_professionnelle = $reduction_professionnelle;
+
+        return $this;
+    }
+}
